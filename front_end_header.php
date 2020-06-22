0b7e50ff6c9c0ed3c0f13f18cb686c9c82b615ef
sheraz Ali <67253160+sherazhunzai1@users.noreply.github.com>
2020-06-22 09:32:03 +0500

    Create front_end_header.php

---
+++ front_end_header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Home</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url();  ?>assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();  ?>assets/css/custom.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+    <script src="<?php echo base_url(); ?>assets/js/jquery-1.11.0.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+<nav class="navbar">
+  <div class="container-fluid navbar-fixed-top header">
+    <div class="row main-header">
+       <div class="navbar-header">
+       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo base_url(); ?>Home">GigeconoMe</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+    <form class="navbar-form navbar-left" action="/action_page.php">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+     
+    </form>
+   
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"> <img src="<?php echo base_url();  ?>assets/images/header/message.png"  alt="..."></a></li>
+       <li><a href="#"> <img src="<?php echo base_url();  ?>assets/images/header/n.png"  alt="..."></a></li>
+      
+      <li class="user"><a href="#"><img class="img-circle" src="<?php echo base_url();  ?>assets/images/pic.png"  alt="...">
+         <span> Welcome <b>Hassan</b></span></a></li>
+    </ul>
+    </div>
+    
+    </div>
+   
+  
+
+
+  <div class="row navigate">
+  <div class="col-md-2">
+    <a href="<?php echo base_url();  ?>Home/Projects"><h4 align="center">Writing & Translation</h4></a>
+  </div>
+    <div class="col-md-2 col-sm-12">
+    <a href="#"><h4 align="center">Writing & Translation</h4></a>
+  </div>
+  <div class="col-md-2">
+    <a href="#"><h4 align="center">Writing & Translation</h4></a>
+  </div>
+  <div class="col-md-2">
+    <a href="#"><h4 align="center">Writing & Translation</h4></a>
+  </div>
+  <div class="col-md-2">
+    <a href="#"><h4 align="center">Writing & Translation</h4></a>
+  </div>
+  <div class="col-md-2">
+    <a href="#"><h4 align="center">Writing & Translation</h4></a>
+  </div>
+
+</div>
+</div>
+</nav>
+//end header
